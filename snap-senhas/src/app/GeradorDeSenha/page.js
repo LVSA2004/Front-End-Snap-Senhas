84d75516e60877aab0cb5da91cdf4ff712345593
Luan Sá <101113009+LVSA2004@users.noreply.github.com>
2023-08-19 20:29:17 -0300

    Add files via upload

---
+++ snap-senhas/src/app/GeradorDeSenha/page.js
@@ -0,0 +1,42 @@
+
+
+export default function GeradorDeSenha() {
+    return (
+
+        <div class="relative">
+        <img class="h-screen w-screen" src="https://cdn.discordapp.com/attachments/970834305305694218/1141838829750321272/image.png" alt="background" />
+        <div class="TelaQueExibeASenhaGerada w-150 h-50 left-[750px] top-[30px] inline-block absolute">
+            <div class="Rectangle14 w-[420px] h-[196px] bg-orange-300 rounded-[25px]"></div>
+            <div class="Yic5aIthe9ipi w-80 h-12 left-[36.40px] top-[73.50px] absolute text-center text-black text-3xl font-semibold">&yIC5a!ItHe9ipI</div>
+        </div>
+        <div class="CaracterSticasDaSenha left-[680px] top-[250px] w-150 h-96 absolute">
+            <div class="Rectangle15 w-[548px] h-[382px] left-[90px] top-[105.73px] rounded-3xl opacity-50 bg-zinc-300"></div>
+    
+            <div class="Group22 w-24 h-11 left-[433.47px] top-[105.73px] absolute">
+                <div class="Rectangle13 w-24 h-11 bg-lime-800 rounded-3xl"></div>
+                <div class="Ellipse1 w-11 h-10 left-[47.58px] top-[1.71px] absolute bg-zinc-300 rounded-full"></div>
+            </div>
+            <div class="Group28 w-24 h-11 left-[431.70px] top-[173.95px] absolute">
+                <div class="Rectangle13 w-24 h-11 bg-lime-800 rounded-3xl"></div>
+                <div class="Ellipse1 w-11 h-10 left-[47.58px] top-[1.71px] absolute bg-zinc-300 rounded-full"></div>
+            </div>
+            <div class="Group30 w-24 h-11 left-[433.47px] top-[310.38px] absolute">
+                <div class="Rectangle13 w-24 h-11 bg-lime-800 rounded-3xl"></div>
+                <div class="Ellipse1 w-11 h-10 left-[47.58px] top-[1.71px] absolute bg-zinc-300 rounded-full"></div>
+            </div>
+            <div class="Group29 w-24 h-11 left-[435.23px] top-[242.16px] absolute">
+                <div class="Rectangle13 w-24 h-11 bg-lime-800 rounded-3xl"></div>
+                <div class="Ellipse1 w-11 h-10 left-[47.58px] top-[1.71px] absolute bg-zinc-300 rounded-full"></div>
+            </div>
+            <div class="Line1 w-60 h-px left-[273.12px] top-[44.34px] absolute border border-black"></div>
+            <div class="Comprimento15 w-56 h-8 left-[21.14px] top-[28.99px] absolute text-center text-white text-2xl font-semibold">Comprimento: 15</div>
+            <div class="Ellipse2 w-5 h-5 left-[303.07px] top-[34.11px] absolute bg-black rounded-full"></div>
+            <div class="Letras w-28 h-7 left-[27px] top-[113px] absolute text-center text-white text-3xl font-semibold">Letras</div>
+            <div class="MinSculasEMaiSculas w-80 h-8 left-[27px] top-[172px] absolute text-center text-white text-2xl font-semibold">Minúsculas e Maiúsculas</div>
+            <div class="NMeros w-36 h-8 left-[27px] top-[235px] absolute text-center text-white text-3xl font-semibold">Números</div>
+            <div class="CaracteresEspeciais w-64 h-8 left-[27px] top-[309px] absolute text-center text-white text-2xl font-semibold">Caracteres Especiais</div>
+        </div>
+    </div>
+    
+    )
+}
